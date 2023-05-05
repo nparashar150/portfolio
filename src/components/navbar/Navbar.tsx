16a1ce666a87079868ca72fe52766175b44f55eb
nparashar150 <nparashar150@gmail.com>
2023-05-05 10:03:43 +0530

    update nav badge

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -10,13 +10,21 @@ export default component$(() => {
         "pl-20 flex justify-between items-center border-b-[1px] border-matte w-[100vw] -ml-20"
       }
     >
-      <div class={"pt-10 pb-8 text-[40px] text-matte"}>
+      <div
+        onClick$={() => (window.location.pathname = "/")}
+        class={"cursor-pointer pt-10 pb-8 text-[36px] text-matte"}
+      >
         Naman <span class={"-ml-2 text-green"}>.</span>
       </div>
       <div class={"w-6/12 flex justify-between items-center"}>
         {NAVBAR_CONFIG.map((item) => {
           return (
-            <div key={item.route} class={"pt-10 pb-8 text-[16px] font-WorkSans transition-all ease-in-out duration-300 hover:text-darkGreen"}>
+            <div
+              key={item.route}
+              class={
+                "pt-10 pb-8 text-[16px] font-WorkSans transition-all ease-in-out duration-300 hover:text-darkGreen"
+              }
+            >
               <a href={item.route}>{item.label}</a>
             </div>
           );
@@ -24,18 +32,18 @@ export default component$(() => {
         <div class={"border-l-[1px] py-9 px-6 min-w-[13%]"}>
           {!isDarkTheme.value ? (
             <img
-            alt={"Theme Toggle"}
-            class={"cursor-pointer -mb-2"}
-            src={"/theme-toggle-to-dark.svg"}
-            onClick$={() => isDarkTheme.value = !isDarkTheme.value}
-          />
+              alt={"Theme Toggle"}
+              class={"cursor-pointer -mb-2"}
+              src={"/theme-toggle-to-dark.svg"}
+              onClick$={() => (isDarkTheme.value = !isDarkTheme.value)}
+            />
           ) : (
             <img
-            alt={"Theme Toggle"}
-            class={"cursor-pointer -mb-2"}
-            src={"/theme-toggle-to-light.svg"}
-            onClick$={() => isDarkTheme.value = !isDarkTheme.value}
-          />
+              alt={"Theme Toggle"}
+              class={"cursor-pointer -mb-2"}
+              src={"/theme-toggle-to-light.svg"}
+              onClick$={() => (isDarkTheme.value = !isDarkTheme.value)}
+            />
           )}
         </div>
       </div>
