10aaa77a6a1bf1939dca8cae18fa141b4ba7ba0b
nparashar150 <nparashar150@gmail.com>
2023-06-12 07:32:39 +0530

    allow sending target via props in <Link />

--- src/components/link/Link.tsx
+++ src/components/link/Link.tsx
@@ -2,11 +2,12 @@ import { Slot, component$ } from "@builder.io/qwik";
 
 interface LinkProps {
   href: string;
+  target?: "_self" | "_blank" | "_parent" | "_top";
 }
 
-export default component$<LinkProps>(({ href }) => {
+export default component$<LinkProps>(({ href, target = "_blank" }) => {
   return (
-    <a target="_blank" rel="noreferrer" href={href} class={"group group flex cursor-pointer items-center justify-center transition-all duration-300 ease-in-out hover:text-darkGreen dark:hover:text-green"}>
+    <a target={target} rel="noreferrer" href={href} class={"group group flex cursor-pointer items-center justify-center transition-all duration-300 ease-in-out hover:text-darkGreen dark:hover:text-green"}>
       <Slot />
       <img class={"group-hover:rotate-12 dark:invert"} src="/top-arrow-right.svg" alt="arrow" />
     </a>
