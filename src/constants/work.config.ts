1fbe8ea5e8244cc160db69713e163a1dd3aacc1d
nparashar150 <nparashar150@gmail.com>
2023-08-21 09:22:39 +0530

    add sylva's description

--- src/constants/work.config.ts
+++ src/constants/work.config.ts
@@ -1,18 +1,25 @@
 export default [
   {
-    heading: "Antler",
-    description:
-      "Antler, a $2B early-stage VC firm, has supported 3,000 aspiring founders in starting 500 companies worldwide. As part of my work with Antler, I streamlined and consolidated the employee onboarding process, developed an internal product for all founders, automated bugs/tickets management and broken data migration processes, and fixed bugs and added features to the Antler website and internal products such as Antler Hub, Antler Fusion, and Antler Demoday.",
+    heading: "Sylva",
+    description: "Sylva is on a mission to build belonging by operating and nurturing niche online communities. As a part of my work with Sylva, I gained insights into multi-tenant architectures and enhanced skills through no-code tools. Addressed data discrepancies across multiple platforms and actively involved in the internal project.",
     defaultState: true,
   },
   {
+    heading: "Antler",
+    description:
+      "Antler, a $2B early-stage VC firm, has supported 3,000 aspiring founders in starting 500 companies worldwide. As part of my work with Antler, I streamlined and consolidated the employee onboarding process, developed an internal product for all founders, automated bugs/tickets management and broken data migration processes, and fixed bugs and added features to the Antler website and internal products such as Antler Hub, Antler Fusion, and Antler Demoday.",
+    defaultState: false,
+  },
+  {
     heading: "OffsetFarm",
-    description: "OffsetFarm's platform simplifies financing for climate action by allowing investors to design their own deals to finance projects and earn carbon offsets. As part of my work with the company, I refactored and optimized the existing codebase for their website, resulting in faster load times and improved performance, as well as revamping the user interface to create a more intuitive design. Additionally, I designed and developed app.offsetfarm.io, an app for financing projects and earning carbon offsets. I also collaborated with the team on product-related decisions and contributed to the overall strategy of the company.",
+    description:
+      "OffsetFarm's platform simplifies financing for climate action by allowing investors to design their own deals to finance projects and earn carbon offsets. As part of my work with the company, I refactored and optimized the existing codebase for their website, resulting in faster load times and improved performance, as well as revamping the user interface to create a more intuitive design. Additionally, I designed and developed app.offsetfarm.io, an app for financing projects and earning carbon offsets. I also collaborated with the team on product-related decisions and contributed to the overall strategy of the company.",
     defaultState: false,
   },
   {
     heading: "Care.Fi",
-    description: "Care.fi is a fintech startup providing smart financing solutions to hospitals, doctors, and allied suppliers. As part of my work with the company, I built the frontend for CareCred from scratch, creating an intuitive and user-friendly design to make accessing financing solutions easier for medical care suppliers. I also mentored new team members, contributed to the development of new products, and collaborated with the team on product decisions, providing insights into the needs and preferences of medical care suppliers.",
+    description:
+      "Care.fi is a fintech startup providing smart financing solutions to hospitals, doctors, and allied suppliers. As part of my work with the company, I built the frontend for CareCred from scratch, creating an intuitive and user-friendly design to make accessing financing solutions easier for medical care suppliers. I also mentored new team members, contributed to the development of new products, and collaborated with the team on product decisions, providing insights into the needs and preferences of medical care suppliers.",
     defaultState: false,
   },
 ];
